dbc3c1ba215a374c34c2a8760f9b54edda853f85
dtrivi <danieltrivinos@gmail.com>
2021-01-21 23:59:25 -0600

    Completed project 1.21.21 11:58pm

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -7,9 +7,8 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -74,8 +73,7 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column);
-
+            String aValue = row.get(column).toLowerCase(); //added .toLowerCase() for case insensitivity requirement.
             if (aValue.contains(value)) {
                 jobs.add(row);
             }
@@ -83,6 +81,35 @@ public class JobData {
 
         return jobs;
     }
+
+    /** findByValue task
+     * want it to iterate through the ArrayList, then through the HashMap objects and check each key/value pair (objects) for the value we pass through (the search term). If an object contains that term, we want to add that job object to an ArrayList of HashMap objects which will make up the results for the user.**/
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+
+        // load data, if not already loaded
+        loadData();
+
+        value = value.toLowerCase();
+
+        ArrayList<HashMap<String, String>> jobRecords = new ArrayList<>();
+        // line 95 creates the ArrayList of HashMaps that will act as results the users sees. This is where the code pushes the matching job records based on the search term.
+        for (int i = 0; i < allJobs.size(); i++) {
+            String aJob = "";
+            for (Map.Entry<String, String> jobListing : allJobs.get(i).entrySet()) {
+                //line 99 states jobListing is of type Map.Entry<String, String>, which "represents the key/value pairs within HashMaps". So jobListing is a HashMap object, and in this case it's one of many objects stored within the allJobs ArrayList. The .get() method gets us the value of one of those objects (the index we're on in our loop at that time) and entrySet() groups the keys and values together from that index point.
+                aJob = aJob + jobListing.getValue();
+                //line 101 now adds to the empty string for aJob with the value side of the key/value pairs (ex: employer(key): Splitwise(value)) from the jobListing HashMap for each iteration (via concatenation). These are the values that are checked against the value argument we pass through (searchTerm).
+            }
+            aJob = aJob.toLowerCase();
+            //moved .toLowerCase() outside of loop
+
+            if (aJob.contains(value)) {
+                jobRecords.add(allJobs.get(i));
+            } //had to move the conditional statement in line 107 out of the HashMap loop to prevent duplicates. Initialized and declared aJob outside of the HashMap loop (as opposed to inside) as a result.
+
+        }
+        return jobRecords;
+    };
 
     /**
      * Read in data from a CSV file and store it in a list
--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -61,7 +62,8 @@ public class TechJobs {
                 String searchTerm = in.nextLine();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    printJobs(JobData.findByValue(searchTerm));
+//                    System.out.println("Search all fields not yet implemented.");
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -110,7 +112,18 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            //findColumnsAndValues checks the search term by the column the user specifies then builds and returns a list of all matching results of the search criteria. Line 115 checks to see how many results are populated by the user's search. If it's zero, it prompts the user to try again.
+            System.out.println("No results. Try again.");
+            } else {
+            for (int i = 0; i < someJobs.size(); i++) {
+                System.out.println("*****");
+                for (Map.Entry<String, String> job : someJobs.get(i).entrySet()) {
+                    System.out.println(job.getKey() + ": " + job.getValue());
+                }
+                System.out.println("*****\n");
+            }
+        }
+//        System.out.println("printJobs is not implemented yet");
     }
 }
